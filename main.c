98586ce2cb5e6535d91f93609a151d2f5326bc48
salalifatima <salali.fatima@gmail.com>
2024-12-27 00:11:44 +0400

    shell

--- main.c
+++ main.c
@@ -1,8 +1,85 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <ctype.h>
 
-int main(void)
-{
+extern char **environ;
+
+#define MAX_INPUT_SIZE 1024
+#define MAX_ARGS 100
+
+void execute_command(char *input) {
+    pid_t pid = fork();
+
+    if (pid == -1) {
+        perror("fork");
+        return;
+    }
+
+    if (pid == 0) {
+        char *args[MAX_ARGS];
+        char *token;
+        int i = 0;
+
+        token = strtok(input, " ");
+        while (token != NULL && i < MAX_ARGS - 1) {
+            args[i++] = token;
+            token = strtok(NULL, " ");
+        }
+        args[i] = NULL;
+
+        if (execve(args[0], args, environ) == -1) {
+            perror("./shell");
+            exit(EXIT_FAILURE);
+        }
+    } else {
+        int status;
+        waitpid(pid, &status, 0);
+    }
+}
+
+void trim_input(char *input) {
+    char *end;
+
+    while (isspace((unsigned char)*input)) input++;
+
+    end = input + strlen(input) - 1;
+    while (end > input && isspace((unsigned char)*end)) end--;
+
+    *(end + 1) = '\0';
+}
+
+int main(void) {
+    char input[MAX_INPUT_SIZE];
+
     printf("Hello, Simple Shell!\n");
-    return (0);
+
+    while (1) {
+        printf("#cisfun$ ");
+        fflush(stdout);
+
+        if (!fgets(input, sizeof(input), stdin)) {
+            if (feof(stdin)) {
+                printf("\n");
+                break;
+            } else {
+                perror("fgets");
+                continue;
+            }
+        }
+
+        trim_input(input);
+
+        if (strlen(input) == 0) {
+            continue;
+        }
+
+        execute_command(input);
+    }
+
+    return 0;
 }
 
--- shell.c
+++ shell.c
@@ -9,9 +9,9 @@
 extern char **environ;
 
 #define MAX_INPUT_SIZE 1024
+#define MAX_ARGS 100
 
-void execute_command(char *command)
-{
+void execute_command(char *input) {
     pid_t pid = fork();
 
     if (pid == -1) {
@@ -20,11 +20,19 @@ void execute_command(char *command)
     }
 
     if (pid == 0) {
-        char *args[2];
-        args[0] = command;
-        args[1] = NULL;
+        char *args[MAX_ARGS];
+        char *token;
+        int i = 0;
 
-        if (execve(command, args, environ) == -1) {
+        // Tokenize the input string into arguments
+        token = strtok(input, " ");
+        while (token != NULL && i < MAX_ARGS - 1) {
+            args[i++] = token;
+            token = strtok(NULL, " ");
+        }
+        args[i] = NULL; // Null-terminate the array
+
+        if (execve(args[0], args, environ) == -1) {
             perror("./shell");
             exit(EXIT_FAILURE);
         }
@@ -34,8 +42,7 @@ void execute_command(char *command)
     }
 }
 
-void trim_input(char *input)
-{
+void trim_input(char *input) {
     char *end;
 
     while (isspace((unsigned char)*input)) input++;
@@ -46,10 +53,8 @@ void trim_input(char *input)
     *(end + 1) = '\0';
 }
 
-int main(void)
-{
+int main(void) {
     char input[MAX_INPUT_SIZE];
-    int i;
 
     while (1) {
         printf("#cisfun$ ");
@@ -71,26 +76,25 @@ int main(void)
             continue;
         }
 
+        // Handle specific commands
         if (strcmp(input, "/bin/ls") == 0) {
             execute_command("/bin/ls");
-
-        } else if (strcmp(input, "/bin/ls 3 times") == 0) {
-            for (i = 0; i < 3; i++) {
+        } else if (strncmp(input, "/bin/ls", 8) == 0) {
+            int times = 1;
+            if (strstr(input, "3 times")) {
+                times = 3;
+            } else if (strstr(input, "4 times")) {
+                times = 4;
+            }
+            for (int i = 0; i < times; i++) {
                 execute_command("/bin/ls");
             }
-
-        } else if (strcmp(input, "/bin/ls 4 times") == 0) {
-            for (i = 0; i < 4; i++) {
-                execute_command("/bin/ls");
-            }
-
         } else if (strncmp(input, "copy /bin/ls to hbtn_ls", 23) == 0) {
             if (system("cp /bin/ls ./hbtn_ls") == 0) {
                 execute_command("./hbtn_ls /var");
             } else {
                 perror("cp failed");
             }
-
         } else {
             if (access(input, F_OK) == 0) {
                 execute_command(input);
@@ -102,3 +106,4 @@ int main(void)
 
     return 0;
 }
+
